c210fea021ce8ec9f85075b19a1bff22fb5c1c88
Satheeshkumar P <satheesh@trendlyne.com>
2024-04-08 20:56:00 +0530

    added nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,8 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
+import "../global.css"
+
 import { useColorScheme } from '@/components/useColorScheme';
 
 export {
